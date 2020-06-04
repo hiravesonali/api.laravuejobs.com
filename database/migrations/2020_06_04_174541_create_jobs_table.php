8b8e7630621d8a78ea16e50a45f301f4a45b6a10
Sonali Hirave <hiravesonali@gmail.com>
2020-06-04 20:36:20 +0200

    migrations added

---
+++ database/migrations/2020_06_04_174541_create_jobs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('company_id');
+            $table->enum('type', ['full time', 'part time', 'both'])->default('full time');
+            $table->boolean('is_remote')->default(false);
+            $table->unsignedBigInteger('location_id');
+            $table->string('contact_person')->nullable();
+            $table->string('contact_person_email')->nullable();
+            $table->string('apply_url');
+            $table->string('salary')->nullable();
+            $table->text('description');
+            $table->dateTime('published_at');
+            $table->timestamps();
+
+
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
